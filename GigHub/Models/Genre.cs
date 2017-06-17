9fb673a285f9c47025366ddf61373007b689b57f
9utella <9utell@gmail.com>
2017-06-17 08:45:40 -0700

    Create domain models

---
+++ GigHub/Models/Genre.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GigHub.Models
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+    }
+}
